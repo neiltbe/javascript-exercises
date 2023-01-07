c1e1abaceba11d250738d633c25e2908705ad749
Neil Bhasin <nbhasin7@yahoo.com>
2023-01-06 22:19:10 -0500

    solves problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let word = string;
+let number = num;
+let blank = ""
+if (num == 0) {
+    return ""
+}
 
+if (num < 1) {
+    return "ERROR"
+}
+for (let i = 0; i < number; i++) {
+blank += word;
+}
+return blank;
 };
 
 // Do not edit below this line
