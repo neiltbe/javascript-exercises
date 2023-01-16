7966abb685d1cfcb455d42e44fbddd9601d3a67c
Neil Bhasin <nbhasin7@yahoo.com>
2023-01-16 01:19:50 -0500

    solves tempcoversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+degree = Number(degree);
+let celsius =  (5/9) * (degree - 32)
+if (celsius == 0.0)
+{
+return 0;
+}
+else
+{
+return Number(celsius.toFixed(1));
+}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+degree = Number(degree);
+let f = (degree * (9/5)) + 32;
+if (f == 0.0){
+return 0;
+}
+else {
+return Number(f.toFixed(1));
+}
 };
 
 // Do not edit below this line
